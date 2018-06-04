1805954c611bd7e1501892eeac0b09a513ebb4a5
Iain MacLennan <Iain@iains-mbp.home>
2018-06-04 22:02:22 +0100

    First commit

---
+++ YoutubeJS/JSBasics/funct.js
@@ -0,0 +1,14 @@
+
+
+let sayHello = function (user){
+    console.log('Message for ' + user);
+    console.log(`HEY ${user}`);
+}
+
+sayHello("Floppy");
+
+let fullNameMaker = function(firstname, lastname) {
+    console.log(`Welcome to LCO,  ${firstname} ${lastname}`);
+}
+
+fullNameMaker('John', 'Smith');
---
+++ YoutubeJS/JSBasics/introArrray.js
@@ -0,0 +1,14 @@
+//var actvityBase = [running,swimming,cycling,workout,weights]
+
+var gv = 'Outside of block';
+
+if (true) {
+    console.log(gv)
+
+   let lv ="lv"
+    
+}
+
+console.log(gv)
+console.log(lv)
+
